34594cc9214b8b88990aeb3c81f830bbfd039b6d
ggipk <ggipk@naver.com>
2021-08-13 20:59:47 +0900

    리액트 시작

--- front/layouts/App.tsx
+++ front/layouts/App.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, {FC} from "react";
+import {Redirect, Route, Switch} from 'react-router-dom';
+import LogIn from '@pages/LogIn';
+import SignUp from '@pages/SignUp';
 
 const App = () => {
     return (
-        <div>
-            폭력 쥬드 물러가라
-        </div>
+        <Switch>
+            <Redirect exact path='/' to="/login" />{/*다른페이지로 돌려주는 역활*/}
+            <Route path="/login" component={LogIn}/>
+            <Route path="/signup" component={SignUp}/>
+        </Switch>
     );
 }
 
---
+++ front/pages/SignUp/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SignUp = ()=>{
+    return (
+        <div>회원쥬드</div>
+    );
+}
+
+export default SignUp;
