40bba5b93c254d5a05d25948a8ecdd41de2ffa6a
ggipk <ggipk@naver.com>
2021-08-15 16:24:29 +0900

    회원가입 시작

--- front/layouts/App.tsx
+++ front/layouts/App.tsx
@@ -1,12 +1,14 @@
 import React, {FC} from "react";
 import {Redirect, Route, Switch} from 'react-router-dom';
-import LogIn from '@pages/LogIn';
-import SignUp from '@pages/SignUp';
+import loadable from "@loadable/component";
+
+const LogIn = loadable(() => import('@pages/LogIn'));
+const SignUp = loadable(() => import('@pages/SignUp'));
 
 const App = () => {
     return (
         <Switch>
-            <Redirect exact path='/' to="/login" />{/*다른페이지로 돌려주는 역활*/}
+            <Redirect exact path='/' to="/login"/>{/*다른페이지로 돌려주는 역활*/}
             <Route path="/login" component={LogIn}/>
             <Route path="/signup" component={SignUp}/>
         </Switch>
