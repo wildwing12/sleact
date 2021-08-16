1b04542b64796dca7d9786b8b6019e763b0a00d4
ggipk <ggipk@naver.com>
2021-08-16 16:16:15 +0900

    로그인까지

--- front/pages/LogIn/index.tsx
+++ front/pages/LogIn/index.tsx
@@ -1,9 +1,51 @@
-import React from "react";
+import useInput from '@hooks/useInput';
+import { Form, Error, Label, Input, LinkContainer, Button, Header } from '@pages/SignUp/styles';
+//import fetcher from '@utils/fetcher';
+import axios from 'axios';
+import React, { useCallback, useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+//import useSWR from 'swr';
 
-const LogIn = ()=>{
+const LogIn = () => {
+    const [logInError, setLoginError]=useState(false);
+    const [email,onChangeEmail] = useInput('');
+    const [password, onChangePassword]=useInput('');
+
+    const onSubmit = useCallback((e)=>{
+        e.preventDefault();
+        setLoginError(false);
+        axios.post('http://localhost:3095/api/users/login',{email,password},{withCredentials:true})
+            .then(()=>{
+                // revalidate();
+            }).catch((error)=>{
+                setLoginError(error.response?.data?.status === 401);
+        })
+    },[email, password])
     return (
-      <div>로그인</div>
+        <div id="container">
+            <Header>Sleact</Header>
+            <Form onSubmit={onSubmit}>
+                <Label id="email-label">
+                    <span>이메일 주소</span>
+                    <div>
+                        <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail} />
+                    </div>
+                </Label>
+                <Label id="password-label">
+                    <span>비밀번호</span>
+                    <div>
+                        <Input type="password" id="password" name="password" value={password} onChange={onChangePassword} />
+                    </div>
+                    {logInError && <Error>이메일과 비밀번호 조합이 일치하지 않습니다.</Error>}
+                </Label>
+                <Button type="submit">로그인</Button>
+            </Form>
+            <LinkContainer>
+                아직 회원이 아니신가요?&nbsp;
+                <Link to="/signup">회원가입 하러가기</Link>
+            </LinkContainer>
+        </div>
     );
-}
+};
 
 export default LogIn;
